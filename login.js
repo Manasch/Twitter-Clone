73c255f96f96e9d574dbe3f51a142069f59bab46
AthShaGad <79466218+AthShaGad@users.noreply.github.com>
2021-12-08 18:42:18 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -25,47 +25,50 @@ class Register extends React.Component{
     }
     
     render(){
-        var header ={
-            backgroundColor:"blue",
-            padding:10,
-            fontFamily:"Arial",
-            marginTop: 0,
-            marginLeft:0,
-            color:"White",
-            textAlign:"center",
-            width:"100%",
-            display:"table",
-            position:"absolute"
-        }
-        var form={
-            //display:"inline-block",
-            marginLeft:"35%",
-            marginTop:"15%",
-            //border: "10px solid black",
-            paddingRight: 150,
-            paddingTop:50,
-            paddingDown: 50,
-            paddingLeft:150,
-            position:"absolute",
-            color:"crimson",
-            backgroundColor:"#F0F8FF",
-            borderRadius: "10px",
-            boxShadow: "5px 5px 10px black"
-        }
+        // var header ={
+        //     backgroundColor:"blue",
+        //     padding:10,
+        //     fontFamily:"Arial",
+        //     marginTop: 0,
+        //     marginLeft:0,
+        //     color:"White",
+        //     textAlign:"center",
+        //     width:"100%",
+        //     display:"table",
+        //     position:"absolute"
+        // }
+        // var form={
+        //     //display:"inline-block",
+        //     marginLeft:"35%",
+        //     marginTop:"15%",
+        //     //border: "10px solid black",
+        //     paddingRight: 150,
+        //     paddingTop:50,
+        //     paddingDown: 50,
+        //     paddingLeft:150,
+        //     position:"absolute",
+        //     color:"crimson",
+        //     backgroundColor:"rgb(238, 224, 241)",
+        //     borderRadius: "10px",
+        //     boxShadow: "5px 5px 10px black"
+        // }
 
-        var main={
-            marginTop:0, 
-            backgroundColor:"black"
-        }
+        // var main={
+        //     marginTop:0, 
+        //     backgroundColor:"black"
+        // }
         return(
-            <div style={main}>
-                <div style={header}><h1>Twitter</h1></div><br/>
-                <div style={form}>
+            <div  className="main">
+                <div  className="header"><h1>Raven</h1></div>
+                <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                <div  className="form">
+                    <br/>
                     <form onSubmit={this.handleSubmit}>
-                        
+                        <div className="myClass">
                         <label>Enter your Username: </label><br/><input type="text" defaultValue="Username" onChange= {this.handleChange}ref= {this.username}/><br/><br/>
                         <label>Enter your Password: </label><br/><input type="text" defaultValue="Password" onChange= {this.handleChange} ref={this.password}/><br/><br/>
-                       <input type="button" value="submit"/>
+                       <input type="button" className="button-74"value="submit"/>
+                       </div>
                     </form>
                 </div>
             </div>
@@ -73,5 +76,6 @@ class Register extends React.Component{
     }
 }
 
-ReactDOM.render(<Register/>,
-    document.getElementById("root"))
+// ReactDOM.render(<Register/>,
+//     document.getElementById("root"))
+export default Register
