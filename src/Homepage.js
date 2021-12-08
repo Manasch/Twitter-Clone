bb9af17ced11122f783bae04a3ed081749de22cc
Manas Ch <manas1606@gmail.com>
2021-12-08 15:26:05 +0530

    Temp Commit

--- src/Homepage.js
+++ src/Homepage.js
@@ -7,7 +7,7 @@ import './Homepage.css';
 class Button extends React.Component {
     render() {
         return (
-            <button>{this.props.value}</button>
+            <button className={this.props.className}>{this.props.value}</button>
         )
     }
 }
@@ -15,14 +15,16 @@ class Button extends React.Component {
 class Home extends React.Component {
     render() {
         return (
-            <div className="Home" {...this.props}>
-                <div className="Top">
-                    <img id="bg" src={bg} alt="Background" title="BG"/>
-                    <img id="logo" src={logo} alt="Eagle Icon" title="Icon"/>
+            <div className="home" {...this.props}>
+                <div className="split top">
+                    <img className="bg" src={bg} alt="Background" title="BG"/>
+                    <img className="logo" src={logo} alt="Eagle Icon" title="Icon"/>
+                    <h2 className="title">Raven</h2>
                 </div>
-                <Button className="sign-up" value="Sign Up"/>
-                <Button classname="sign-in" value="Sign In"/>
-                <h1> test </h1>
+                <div className='split bottom'>
+                    <Button className="sign-up" value="Sign Up"/>
+                    <Button classname="sign-in" value="Sign In"/>
+                </div>
             </div>
         )
     }
